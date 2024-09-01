f3c4eef89278f8d700ae38d42489dabad9e98102
dhruvik-variya <dhruvikvariya@gmail.com>
2024-09-01 17:49:07 +0530

    js

--- Dragon project/script.js
+++ Dragon project/script.js
@@ -1,7 +1,9 @@
 let user = [];
 
 function handledata(e){
+
     e.preventDefault();
+
     const data = {
         productimg : document.createElementById("Image").value,
         productpname : document.createElementById("Name").value,
@@ -16,25 +18,51 @@ function handledata(e){
     user.push(data);
     Uimaker();
 
+
 };
 
-function Uimaker(){
+const Uimaker = (e) =>{
+
+    e.preventDefault();
+
     document.getElementById("products").innerHTML = "";
 
     for(let i = 0; i < user.length; i++){
 
-        let image = document.createElement("Image");
-        let name = document.createElement("Name");
-        let price = document.createElement("Price");
-        let buy = document.createElement("button");
-        let delet = document.createElement("button");
+         let image = document.createElement("img");
+         image.src = user[i].image;
 
 
-    
-        let tr = document.createElement("tr");
+         let name = document.createElement("h2");
+        name.innerHTML = user[i].name;
+
+         let price = document.createElement("h3");
+         price.innerHTML = "$" + user[i].price;
+
         
+
+         let buy = document.createElement("button");
+         buy.innerHTML = "Buy";
+
+         let delet = document.createElement("button");
+         delet.innerHTML = "Delete";
+        
+
+         let parent = document.createElement("div");
+
+         div.append(image,name, price, buy, delet);
+         document.getElementById("products").append(parent);
+         delet.addEventListener("click", () => {
+             user.splice(i, 1);
+             Uimaker();
+         });
+
+
+         console.log(user[i].price);
+         
     }
 }
+
 
 
 
